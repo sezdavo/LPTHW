65f40d03f2931856914da8856f06e3851ad06d53
sezdavo <50488737+sezdavo@users.noreply.github.com>
2019-05-10 14:14:46 +0100

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,24 @@
+# this line prints a string
+print("I will now count my chickens:")
+# this line prints a string followed by the result of a sum
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# this line prints a string
+print("Now I will count the eggs:")
+# this line prints a sum
+print( 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+# this line prints a string
+print("Is it true that 3 + 2 < 5 - 7?")
+# this line returns a boolean value of an expression
+print(3.0 + 2.0 < 5.0 - 7.0)
+# this line prints a string followed by the result of a sum
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+# this line prints a string
+print("Oh, that's why i'ts False.")
+# this line prints a string
+print("How about some more.")
+# this line prints a string followed by a boolean value of an expression
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
